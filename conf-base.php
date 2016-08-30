04a79fbedae404bd3ccecf108744015a92e02210
Frederick Silva <fred@thehouseofsilva.com>
2016-08-30 11:53:00 -0300

    fixed broke merge

--- conf-base.php
+++ conf-base.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-//    // latitude,longitude do centro do mapa da busca e do mapa da criação de agentes e espaços
-//    'maps.center' => [-14.2400732, -53.1805018],
+   // latitude,longitude do centro do mapa da busca e do mapa da criação de agentes e espaços
+   'maps.center' => [-19.7884002, -43.94023440000001],
 //
 //    // a cada quantos pixels se deve criar um cluster no mapa da busca
 //    'maps.maxClusterRadius' => 40,
@@ -10,20 +10,20 @@ return [
 //    // simplificação do shapefile
 //    'maps.geometryFieldQuery' => "ST_SimplifyPreserveTopology(geom, 0.001)",
 //
-//    // zoom padrão do mapa da busca
-//    'maps.zoom.default' => 5,
+   // zoom padrão do mapa da busca
+   'maps.zoom.default' => 5,
 //
-//    // zoom do mapa da single do agente quando este define a posição como aproximada
-//    'maps.zoom.approximate' => 14,
-//
-//    // zoom do mapa da single do agente quando este define a posição como precisa
-//    'maps.zoom.precise' => 16,
-//
-//    // zoom máximo do mapa da busca
-//    'maps.zoom.max' => 18,
-//
-//    // zom mínimo do mapa da busca
-//    'maps.zoom.min' => 5,
+   // zoom do mapa da single do agente quando este define a posição como aproximada
+   'maps.zoom.approximate' => 14,
+
+   // zoom do mapa da single do agente quando este define a posição como precisa
+   'maps.zoom.precise' => 16,
+
+   // zoom máximo do mapa da busca
+   'maps.zoom.max' => 18,
+
+   // zom mínimo do mapa da busca
+   'maps.zoom.min' => 5,
 //    // incluir no mapa as camadas do google?
 //    'maps.includeGoogleLayers' => false,
 //    // descomente para definir quais fivisões geográficas são utilizadas no tema
